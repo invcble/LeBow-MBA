d6951d5058f7d6b8b34a64991b41876700adff88
invcble <imonbera13@gmail.com>
2024-09-02 12:50:24 -0400

    update

--- new.py
+++ new.py
@@ -33,20 +33,17 @@ def merge_columns_with_suffix(df):
 
 df = merge_columns_with_suffix(df)
 
-recode_columns = data_dict_df[data_dict_df['Recode_Ind'] == 1]['New_Q'].tolist()
-# print(recode_columns)
-
 # Subtract 8 from the identified columns to recode
+recode_columns = data_dict_df[data_dict_df['Recode_Ind'] == 1]['New_Q'].tolist()
 for col in recode_columns:
     if col in df.columns:
         df[col] = 8 - df[col]
 
 
-categories = data_dict_df.groupby('Category')['New_Q'].apply(list).to_dict()
-# print(categories)
+################## CATEGORY ##################
 
+categories = data_dict_df.groupby('Category')['New_Q'].apply(list).to_dict()
 network_categories = {key: categories.pop(key) for key in ['N1', 'N2', 'N3', 'N4', 'N5']}
-print(network_categories)
 
 # Average the values of columns grouped by category
 for category, columns in categories.items():
@@ -64,58 +61,27 @@ df.drop(['StartDate', 'EndDate', 'Status', 'IPAddress', 'Progress',
        'ExternalReference', 'LocationLatitude', 'LocationLongitude',
        'DistributionChannel', 'UserLanguage'], axis=1, inplace=True)
 
+# Process Name, Email and other demographics
 df = df.dropna(subset=['Q1', 'Q2'])
-
 df['Name'] = (df['Q1'].str.strip() + ' ' + df['Q2'].str.strip()).str.title()
 df.drop(['Q1', 'Q2'], axis=1, inplace=True)
 
-df.rename(columns={'Q34': 'Email'}, inplace=True)
+df.rename(columns={'Q3': 'MBA program', 'Q34': 'Email', 'Q39': 'Academic years', 'Q40': 'Academic term', 'Q5': 'Gender', 'Q4': 'DOB', 'Q7': 'Profession', 'Q8': 'Work Ex. years'}, inplace=True)
 df.drop_duplicates(subset=['Email'], keep='last', inplace=True)
 
-mba_program_mapping = {
-    1: 'Online MBA',
-    2: 'Malvern MBA (Vanguard)',
-    3: 'University City MBA',
-    4: 'Full-Time MBA'
-}
+mba_program_mapping = {1: 'Online MBA', 2: 'Malvern MBA (Vanguard)', 3: 'University City MBA', 4: 'Full-Time MBA'}
+academic_years_mapping = {1: '2024-2025', 2: '2025-2026', 3: '2026-2027', 4: '2027-2028', 5: '2028-2029'}
+academic_term_mapping = {1: 'Fall Quarter', 2: 'Winter Quarter', 3: 'Spring Quarter'}
+gender_mapping = { 1: 'Male', 2: 'Female', 3: 'Non-binary', 4: 'Other, or do not wish to share'}
 
-academic_years_mapping = {
-    1: '2024-2025',
-    2: '2025-2026',
-    3: '2026-2027',
-    4: '2027-2028',
-    5: '2028-2029'
-}
-
-academic_term_mapping = {
-    1: 'Fall Quarter',
-    2: 'Winter Quarter',
-    3: 'Spring Quarter'
-}
-
-gender_mapping = {
-    1: 'Male',
-    2: 'Female',
-    3: 'Non-binary',
-    4: 'Other, or do not wish to share'
-}
-
-
-df.rename(columns={'Q3': 'MBA program', 'Q39': 'Academic years', 'Q40': 'Academic term', 'Q5': 'Gender', 'Q4': 'DOB', 'Q7': 'Profession', 'Q8': 'Work Ex. years'}, inplace=True)
 df['MBA program'] = df['MBA program'].map(mba_program_mapping)
 df['Academic years'] = df['Academic years'].map(academic_years_mapping)
 df['Academic term'] = df['Academic term'].map(academic_term_mapping)
 df['Gender'] = df['Gender'].map(gender_mapping)
 
 
+################## NETWORK ##################
 
-
-
-print(df)
-print(df.columns)
-
-
-# Extract Email and Q23 related data
 mba_ntwk = df[['Email'] + [col for sublist in network_categories.values() for col in sublist if col in df.columns]].copy()
 
 def calculate_network_means(df, columns, divisor_value):
@@ -125,61 +91,24 @@ def calculate_network_means(df, columns, divisor_value):
     # Avoid division by zero
     return category_count / net_size
 
-# Combine results into a DataFrame
-# mba_ntwk = mba_ntwk[['Email']].copy()
-
 # Corrected calculation for individual Cross-Functional (CF), External Contacts (EC), Higher Levels (HLC) averages
 mba_ntwk['Cross_Func'] = calculate_network_means(mba_ntwk, network_categories['N2'], 2)
 mba_ntwk['Ext'] = calculate_network_means(mba_ntwk, network_categories['N3'], 2)
 mba_ntwk['High_Lvl'] = calculate_network_means(mba_ntwk, network_categories['N4'], 3)
 
-# Calculate overall means for CF, EC, and HLC
-cf_mean = mba_ntwk['Cross_Func'].mean(skipna=True)
-ec_mean = mba_ntwk['Ext'].mean(skipna=True)
-hlc_mean = mba_ntwk['High_Lvl'].mean(skipna=True)
-
-# Add average row
-# average_row = pd.Series(['Average', cf_mean, ec_mean, hlc_mean], index=mba_ntwk.columns)
-# mba_ntwk = pd.concat([mba_ntwk, average_row.to_frame().T], ignore_index=True)
-
-# Display the final wide DataFrame before melting, if needed
-print(mba_ntwk.iloc[:,100:])
-
-
 # Replace values of 2 with 1 specifically in columns corresponding to N1
 n1_columns = network_categories['N1']
 mba_ntwk[n1_columns] = mba_ntwk[n1_columns].replace(2, 1)
 
-# Create the subset DataFrame mba_ntwk_size directly using Email and N1 columns
-mba_ntwk_size = mba_ntwk[['Email'] + n1_columns].copy()
-
 # Calculate network size (non-missing values count)
-mba_ntwk_size['Total_Size'] = mba_ntwk_size.iloc[:, 1:].notna().sum(axis=1)
+mba_ntwk['Total_Size'] = mba_ntwk.iloc[:, 1:21].notna().sum(axis=1)
+mba_ntwk['Weak'] = mba_ntwk.iloc[:, 1:21].apply(lambda row: (row == 1).sum(), axis=1)
+mba_ntwk['Strong'] = mba_ntwk.iloc[:, 1:21].apply(lambda row: (row == 3).sum(), axis=1)
 
-# Calculate weak and strong ties
-mba_ntwk_size['Weak'] = mba_ntwk_size.iloc[:, 1:].apply(lambda row: (row == 1).sum(), axis=1)
-mba_ntwk_size['Strong'] = mba_ntwk_size.iloc[:, 1:].apply(lambda row: (row == 3).sum(), axis=1)
+mba_ntwk = mba_ntwk[['Email', 'Cross_Func', 'Ext', 'High_Lvl', 'Weak', 'Strong', 'Total_Size']]
+columns_to_drop = [col for sublist in network_categories.values() for col in sublist]
+df = df.drop(columns=columns_to_drop)
 
-# Calculate the average total size
-average_total_size = mba_ntwk_size['Total_Size'].mean(skipna=True)
-print(f"Average Total Size: {average_total_size}")
-
-
-
-# Display the final DataFrame
-print(mba_ntwk_size)
-
-# Keep only the specified columns in mba_ntwk
-mba_ntwk = mba_ntwk[['Email', 'Cross_Func', 'Ext', 'High_Lvl']]
-
-# Keep only the specified columns in mba_ntwk_size
-mba_ntwk_size = mba_ntwk_size[['Total_Size', 'Weak', 'Strong']]
-
-# Merge the cleaned DataFrames on the 'Email' column
-mba_ntwk = mba_ntwk.join(mba_ntwk_size, how='outer')
-
-# Display the merged DataFrame
-print(mba_ntwk)
-# print(merged_df)
-
-
+df = pd.merge(df, mba_ntwk, on='Email', how='left').sort_index()
+df = df[['Name'] + [col for col in df.columns if col != 'Name']]
+print(df)
